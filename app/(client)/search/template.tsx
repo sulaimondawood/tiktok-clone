955dcae7f4184d0e5af82602d4c435a07a5cd820
sulaimondawood <sulaimondawod@gmail.com>
2023-11-08 13:29:41 +0100

    new changes

---
+++ app/(client)/search/template.tsx
@@ -0,0 +1,16 @@
+import Button from "@/components/button/Button";
+import React from "react";
+import "@/globals.css";
+import UploadSideBar from "@/components/sidebar/UploadSideBar";
+
+export default function UploadLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <main>
+      <div className="max-w-[1440px]">{children}</div>
+    </main>
+  );
+}
