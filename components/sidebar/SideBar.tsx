60b6ffe801122c952ec347b3d900ab31356398ba
sulaimondawood <sulaimondawod@gmail.com>
2023-11-09 11:56:20 +0100

    working on search page

--- components/sidebar/SideBar.tsx
+++ components/sidebar/SideBar.tsx
@@ -23,7 +23,7 @@ const SideBar = () => {
     setLoading(false);
   }, []);
   return (
-    <div className="lg:w-[250px]  h-screen px-4 overflow-y-auto fixed mt-5 z-50">
+    <div className="lg:w-[250px]  h-screen px-4 overflow-y-hidden hover:overflow-y-auto fixed mt-5 z-50">
       <div className="flex flex-col items-start py-6 border-b border-b-gray-300 justify-center gap-3">
         {sideBar.map((item, index) => {
           return (
