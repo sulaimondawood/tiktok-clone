e9400939d1fb9b2888cf7c36c88d9708caccae1c
sulaimondawood <sulaimondawod@gmail.com>
2023-11-12 16:06:44 +0100

    testing home page

--- app/(client)/following/page.tsx
+++ app/(client)/following/page.tsx
@@ -1,29 +1,29 @@
-// import { VideoSkeleton } from "@/components/skeletons/Skeleton";
-// import VideoCard from "@/components/videocard/VideoCard";
+import { VideoSkeleton } from "@/components/skeletons/Skeleton";
+import VideoCard from "@/components/videocard/VideoCard";
 
-// import { Post } from "@/types/posts";
-// import { URL } from "@/utils/constants/getUsers";
-// import { Suspense } from "react";
+import { Post } from "@/types/posts";
+import { URL } from "@/utils/constants/getUsers";
+import { Suspense } from "react";
 
-// async function getPosts() {
-//   const res = await fetch(`${URL}/api/all-posts-random`, {
-//     cache: "no-store",
-//   });
-//   return res.json();
-// }
+async function getPosts() {
+  const res = await fetch(`${URL}/api/all-posts-random`, {
+    cache: "no-store",
+  });
+  return res.json();
+}
 
-// export default async function Page() {
-//   const posts = await getPosts();
+export default async function Page() {
+  const posts = await getPosts();
 
-//   return (
-//     <>
-//       <Suspense fallback={<VideoSkeleton />}>
-//         <main className="w-full px-3 md:px-0 max-w-3xl  mx-auto ml-[60px] sm:ml-[80px] md:ml-0">
-//           {posts.map((item: Post, index: number) => {
-//             return <VideoCard key={index} post={item} />;
-//           })}
-//         </main>
-//       </Suspense>
-//     </>
-//   );
-// }
+  return (
+    <>
+      <Suspense fallback={<VideoSkeleton />}>
+        <main className="w-full px-3 md:px-0 max-w-3xl  mx-auto ml-[60px] sm:ml-[80px] md:ml-0">
+          {posts.map((item: Post, index: number) => {
+            return <VideoCard key={index} post={item} />;
+          })}
+        </main>
+      </Suspense>
+    </>
+  );
+}
